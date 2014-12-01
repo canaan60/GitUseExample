fb9cc96e644e9e549540468b7c71d5811036197c
canaan60 <walt.schopfer@gmail.com>
2014-11-30 22:09:09 -0500

    Second set of updates

--- GitUseExample/ViewController.m
+++ GitUseExample/ViewController.m
@@ -14,6 +14,13 @@
 
 @implementation ViewController
 
+#pragma mark - IBActions
+
+- (IBAction)clickTheButton:(id)sender
+{
+    NSLog(@"This is a Git tutorial");
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -23,5 +30,4 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 @end
